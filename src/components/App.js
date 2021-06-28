f0e4b02b95ad632d1526f5a032966330aebd1ab3
John <jsjshellz@gmail.com>
2021-06-28 14:41:58 -0600

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "/home/johnshellabarger/Development/Code/Mod2/react-hooks-import-export-lab/src/components/NavBar.js"
+import Home from "/home/johnshellabarger/Development/Code/Mod2/react-hooks-import-export-lab/src/components/Home.js"
+import About from "/home/johnshellabarger/Development/Code/Mod2/react-hooks-import-export-lab/src/components/About.js"
+
 
 function App() {
   return (
